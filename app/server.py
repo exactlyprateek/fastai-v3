b02456f4953a17dc2e01691efbcd08557b3cb3ac
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2020-06-27 17:47:05 +0530

    rollback

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=10cWkPuttjb1sGvDobD3E5BC2vr6MriC8'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['ross', 'rachel', 'joey', 'chandler', 'monica', 'phoebe']
+classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
